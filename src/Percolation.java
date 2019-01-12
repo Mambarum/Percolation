1112c7e04092b4da3d38f4efaa2837e0c8057f32
Mambarum <eerie@ukr.net>
2019-01-12 17:27:51 +0200

    Fixed code style Switched to using methods from algs4.jar

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,108 +1,100 @@
-import java.lang.IllegalArgumentException;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private int[] sites;
-	WeightedQuickUnionUF compounds;
-	private final int sites_num;
-	private final int N;
-	private int open_sites_num = 0;
-	private int virt_node_top;
-	private int virt_node_bot;
-	public Percolation(int n)                // create n-by-n grid, with all sites blocked
-	{
-		if(n <= 0) 
-			throw new IllegalArgumentException("Invalid number of sites specified");
-		
-		compounds = new WeightedQuickUnionUF(n * n + 2);
-		
-		virt_node_top = n * n;
-		virt_node_bot = n * n + 1;
-		
-		sites = new int[n * n];
-		sites_num = n * n;
-		N = n;
-		for(int i = 0; i < sites_num; i++)
-			sites[i] = -1;
-	}
-	
+    private boolean[] sites;
+    private final WeightedQuickUnionUF compounds;
+    private final int totalSitesNum;
+    private final int sitesNum;
+    private int openSitesNum = 0;
+    private final int virtNodeTop;
+    private final int virtNodeBot;
 
-	private void sanity_check(int col, int row)
-	{
-		if( (row <= 0)  ||
-			(row > N) ||
-			(col <= 0)  ||
-			(col > N) ||
-			((row * col) > sites_num))
-				throw new IllegalArgumentException("Invalid site index specified: col = " + col + ", row = " + row);
-	}
-	
-	private int make_index(int row, int col)
-	{
-		return (row * N) + col;
-	}
-	
-	public void open(int row, int col)   // open site (row, col) if it is not open already
-	{
-		sanity_check(col, row);
-		col--;
-		row--;
-		
-		if(check_open(row, col))
-			return;
-		
-		int index = make_index(row, col);
-		open_sites_num++;
-		sites[index] = 0;
-		
-		if(row == 0)
-			compounds.union(index, virt_node_top);
-		else if(row == N - 1)
-			compounds.union(index, virt_node_bot);
-		else
-		{
-			if(check_open(row - 1, col))
-				compounds.union(index, make_index((row - 1), col));
-			
-			if(check_open(row + 1, col))
-				compounds.union(index, make_index((row + 1), col));
-		}
-		
-		if((col != 0) && check_open(row, col - 1))
-			compounds.union(index, make_index(row, (col - 1)));
-		
-		if((col != N - 1) && check_open(row, col + 1))
-			compounds.union(index, make_index(row, (col + 1)));
-	}
-	
-	private boolean check_open(int row, int col)
-	{
-		return (sites[make_index(row, col)] != -1);
-	}
-	
-	public boolean isOpen(int row, int col) // is site (row, col) open?
-	{
-		sanity_check(col, row);
-		col--;
-		row--;
-		return check_open(row, col);
-	}
-	
-	public boolean isFull(int row, int col) // is site (row, col) full?
-	{
-		sanity_check(col, row);
-		col--;
-		row--;
-		return (compounds.find(row * col) == compounds.find(virt_node_top));
-	}
-	
-	public     int numberOfOpenSites()      // number of open sites
-	{
-		return open_sites_num;
-	}
-	
-	public boolean percolates()             // does the system percolate?
-	{
-		return (compounds.find(virt_node_bot) == compounds.find(virt_node_top));
-	}
+    public Percolation(int n) // create n-by-n grid, with all sites blocked
+    {
+        if (n <= 0)
+            throw new IllegalArgumentException("Invalid number of sites specified");
+
+        compounds = new WeightedQuickUnionUF(n * n + 2);
+
+        virtNodeTop = n * n;
+        virtNodeBot = n * n + 1;
+
+        sites = new boolean[n * n];
+        totalSitesNum = n * n;
+        sitesNum = n;
+        for (int i = 0; i < totalSitesNum; i++)
+            sites[i] = false;
+    }
+
+    private void sanityCheck(int col, int row) {
+        if ((row <= 0) || (row > sitesNum) || (col <= 0) || (col > sitesNum) || ((row * col) > totalSitesNum))
+            throw new IllegalArgumentException("Invalid site index specified: col = " + col + ", row = " + row);
+    }
+
+    private int makeIndex(int row, int col) {
+        return (row * sitesNum) + col;
+    }
+
+    public void open(int row, int col) // open site (row, col) if it is not open already
+    {
+        if (isOpen(row, col))
+            return;
+        
+        col--;
+        row--;
+
+        int index = makeIndex(row, col);
+        openSitesNum++;
+        sites[index] = true;
+
+        if (row == 0)
+            compounds.union(index, virtNodeTop);
+        else if (row == sitesNum - 1)
+            compounds.union(index, virtNodeBot);
+        else {
+            if (checkOpen(row - 1, col))
+                compounds.union(index, makeIndex((row - 1), col));
+
+            if (checkOpen(row + 1, col))
+                compounds.union(index, makeIndex((row + 1), col));
+        }
+
+        if ((col != 0) && checkOpen(row, col - 1))
+            compounds.union(index, makeIndex(row, (col - 1)));
+
+        if ((col != sitesNum - 1) && checkOpen(row, col + 1))
+            compounds.union(index, makeIndex(row, (col + 1)));
+    }
+
+    private boolean checkOpen(int row, int col) {
+        return sites[makeIndex(row, col)];
+    }
+
+    public boolean isOpen(int row, int col) // is site (row, col) open?
+    {
+        sanityCheck(col, row);
+        col--;
+        row--;
+        return checkOpen(row, col);
+    }
+
+    public boolean isFull(int row, int col) // is site (row, col) full?
+    {
+        if (!isOpen(row, col))
+            return false;
+        
+        col--;
+        row--;
+        return (compounds.find(row * col) == compounds.find(virtNodeTop));
+    }
+
+    public int numberOfOpenSites() // number of open sites
+    {
+        return openSitesNum;
+    }
+
+    public boolean percolates() // does the system percolate?
+    {
+        return (compounds.find(virtNodeBot) == compounds.find(virtNodeTop));
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,96 +1,73 @@
 import edu.princeton.cs.algs4.StdRandom;
-//import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private final int size;
-	private final int trials;
-	private double mean = 0;
-	private double deviation = 0;
-	private double[] threshold;
-	
-	private double single_run(Percolation perc)
-	{
-		int row, col, i;
-		
-		for(i = 0; i < size * size; i++)
-		{
-			do
-			{
-				row = StdRandom.uniform(1, size + 1);
-				col = StdRandom.uniform(1, size + 1);
-			} while (perc.isOpen(row, col));
-			
-			perc.open(row, col);
-			if(perc.percolates())
-			{
-				System.out.println("The system percolates at " + perc.numberOfOpenSites() + 
-									" open sites (" + (double)perc.numberOfOpenSites()/(size*size) + ")");
-				break;
-			}
-		}
+    private final int size;
+    private final int trials;
+    private final double[] threshold;
 
-		return (double)perc.numberOfOpenSites()/(size*size);
-	}
-	
-	public PercolationStats(int n, int T)    // perform trials independent experiments on an n-by-n grid
-	{
-		double thres_sum = 0;
-		trials = T;
-		size = n;
-		
-		threshold = new double[trials];
-		for(int i = 0; i < trials; i++)
-		{
-			Percolation perc = new Percolation(size);
-			threshold[i] = single_run(perc);
-			thres_sum += threshold[i]; 
-		}
-		
-		mean = thres_sum / trials;
-   }
-	
-   public double mean()                          // sample mean of percolation threshold
-   {
-	   return mean;
-   }
-   
-   public double stddev()                        // sample standard deviation of percolation threshold
-   {
-	   double dev = 0;
-	   
-	   for(int i = 0; i < trials; i++)
-	   {
-		   	double tmp = threshold[i] - mean;
-		   	dev += tmp * tmp;
-	   }
-	   dev = dev/(trials - 1);
-	   deviation = Math.sqrt(dev);
-	   return deviation;
-   }
-   
-   private double five_percent()
-   {
-	   return 1.96 * deviation / Math.sqrt(trials);
-   }
-   
-   public double confidenceLo()                  // low  endpoint of 95% confidence interval
-   {
-	   return mean - five_percent();
-   }
-   
-   public double confidenceHi()                  // high endpoint of 95% confidence interval
-   {
-	   return mean + five_percent();
-   }
-   
-   public static void main(String[] args)        // test client (described below)
-   {
-	   if(args.length < 2)
-		   throw new IllegalArgumentException("Please specify size of the field and number of trials");
-	   
-	   PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-	   System.out.println("mean                    = " + stats.mean());
-	   System.out.println("stddev                  = " + stats.stddev());
-	   System.out.println("95% confidence interval = [" + stats.confidenceLo() + ", "+ stats.confidenceHi() + "]");
-   }
+    public PercolationStats(int n, int trialsIn) // perform trials independent experiments on an n-by-n grid
+    {
+        trials = trialsIn;
+        size = n;
+
+        threshold = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(size);
+            threshold[i] = singleRun(perc);
+        }
+    }
+
+    private double singleRun(Percolation perc) {
+        int row, col, i;
+
+        for (i = 0; i < size * size; i++) {
+            do {
+                row = StdRandom.uniform(1, size + 1);
+                col = StdRandom.uniform(1, size + 1);
+            } while (perc.isOpen(row, col));
+
+            perc.open(row, col);
+            if (perc.percolates()) {
+                break;
+            }
+        }
+
+        return (double) perc.numberOfOpenSites() / (size * size);
+    }
+
+    public double mean() // sample mean of percolation threshold
+    {
+        return StdStats.mean(threshold);
+    }
+
+    public double stddev() // sample standard deviation of percolation threshold
+    {
+        return StdStats.stddev(threshold);
+    }
+
+    private double fivePercent() {
+        return 1.96 * stddev() / Math.sqrt(trials);
+    }
+
+    public double confidenceLo() // low endpoint of 95% confidence interval
+    {
+        return mean() - fivePercent();
+    }
+
+    public double confidenceHi() // high endpoint of 95% confidence interval
+    {
+        return mean() + fivePercent();
+    }
+
+    public static void main(String[] args) // test client (described below)
+    {
+        if (args.length < 2)
+            throw new IllegalArgumentException("Please specify size of the field and number of trials");
+
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
+    }
 }
