d85db72884abcdf6e9132c1bc05bbfd4ac843c5b
Mambarum <eerie@ukr.net>
2019-01-13 13:13:25 +0200

    Added description to the classes and public methods Removed commented code related to the file-based testing

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,15 +1,31 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ *  The {@code Percolation} class estimate the value \
+ *  of the percolation threshold via Monte Carlo simulation. 
+ *  It works with square field of n*n size.
+ *  <p>
+ *  For additional documentation, see
+ *  <a href="http://coursera.cs.princeton.edu/algs4/assignments/percolation.html"></a>
+ *  by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Volodymyr Shanoilo
+ */
 public class Percolation {
-    private boolean[] sites;
-    private final WeightedQuickUnionUF compounds;
-    private final int totalSitesNum;
-    private final int sitesNum;
-    private int openSitesNum = 0;
-    private final int virtNodeTop;
-    private final int virtNodeBot;
+    private boolean[] sites;            // defines if the site is opened or closed
+    private final WeightedQuickUnionUF compounds;   //contains information about compounds 
+    private final int sitesNum;         // n - size of a field
+    private final int totalSitesNum;    // n * n 
+    private int openSitesNum = 0;       // number of currently opened sites
+    private final int virtNodeTop;      // a virtual site that represents all the upper row
+    private final int virtNodeBot;      // a virtual site that represents all the bottom row
 
-    public Percolation(int n) // create n-by-n grid, with all sites blocked
+/**
+ * Creates n * n grid with all sites blocked
+ * Creates default compounds array
+ * @param n - size of the grid
+ */
+    public Percolation(int n)
     {
         if (n <= 0)
             throw new IllegalArgumentException("Invalid number of sites specified");
@@ -25,22 +41,42 @@ public class Percolation {
         for (int i = 0; i < totalSitesNum; i++)
             sites[i] = false;
     }
-
+/**
+ * Checks if input values are within allowed range.
+ * Index range is 1..N
+ * @param col
+ * @param row
+ */
     private void sanityCheck(int col, int row) {
         if ((row <= 0) || (row > sitesNum) || (col <= 0) || (col > sitesNum) || ((row * col) > totalSitesNum))
             throw new IllegalArgumentException("Invalid site index specified: col = " + col + ", row = " + row);
     }
 
+/**
+ * Calculates index in the grid from row and column
+ * @param row
+ * @param col
+ * @return
+ */
     private int makeIndex(int row, int col) {
         return (row * sitesNum) + col;
     }
 
-    public void open(int row, int col) // open site (row, col) if it is not open already
+/**
+ * Opens a cell by provided row and column.
+ * The opened cell is immediately connected to the nearby compound (if exists)
+ * if a cell is opened in the topmost row (row #1) then it is connected
+ * to the virtual top node that effectively unites all compounds that 
+ * exist in the top row. Same works for bottom row (row #N).
+ * @param row
+ * @param col
+ */
+    public void open(int row, int col)
     {
-        if (isOpen(row, col))
+        if (isOpen(row, col)) // if the cell is already opened do nothing
             return;
         
-        col--;
+        col--;  // decrement row and col to cast the to indexes that start from 0
         row--;
 
         int index = makeIndex(row, col);
@@ -55,8 +91,6 @@ public class Percolation {
         if (row == sitesNum - 1)
             compounds.union(index, virtNodeBot);
         else if (checkOpen(row + 1, col))
-//            canPercolate++;
-//        if ((row != sitesNum - 1) && (checkOpen(row + 1, col)))
             compounds.union(index, makeIndex((row + 1), col));
 
         if ((col != 0) && checkOpen(row, col - 1))
@@ -70,47 +104,57 @@ public class Percolation {
         return sites[makeIndex(row, col)];
     }
 
-    public boolean isOpen(int row, int col) // is site (row, col) open?
+/**
+ * Checks if current cell is open
+ * @param row
+ * @param col
+ * @return
+ */
+    public boolean isOpen(int row, int col)
     {
         sanityCheck(col, row);
-        col--;
+        col--;  // decrement row and col to cast the to indexes that start from 0
         row--;
         return checkOpen(row, col);
     }
 
-    public boolean isFull(int row, int col) // is site (row, col) full?
+/**
+ * Checks if a given cell belongs to the full compound.
+ * The compound is full if it contains at least one cell from the topmost row.
+ * Taking into account that all cells at the topmost row are connected to
+ * a virtual top node all we have to do is check if the topmost node
+ * belongs to the same compound as the one being tested.
+ * @param row
+ * @param col
+ * @return
+ */
+    public boolean isFull(int row, int col)
     {
         if (!isOpen(row, col))
             return false;
         
-        col--;
+        col--;  // decrement row and col to cast the to indexes that start from 0
         row--;
         return (compounds.connected(makeIndex(row, col), virtNodeTop));
     }
 
-    public int numberOfOpenSites() // number of open sites
+/**
+ * Returns number of currently opened cells
+ * @return
+ */
+    public int numberOfOpenSites()
     {
         return openSitesNum;
     }
 
-    public boolean percolates() // does the system percolate?
+/**
+ * Checks if current state of the grid percolates.
+ * The grid percolates if at least one of the cells from the bottom row
+ * belongs to a full site. 
+ * @return
+ */
+    public boolean percolates()
     {
         return compounds.connected(virtNodeBot, virtNodeTop);
-/*        if (canPercolate == 0)
-            return false;
-        
-        int tmp = canPercolate;
-        int root = compounds.find(virtNodeTop);
-        
-        for (int col = 0; col < sitesNum; col++) {
-            if (checkOpen(sitesNum - 1, col)) {
-                if (compounds.find(makeIndex(sitesNum - 1, col)) == root)
-                    return true;
-                
-                if (--tmp == 0)
-                    break;
-            }
-        }
-        return false;*/
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,103 +1,63 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-// >>>>>>
-/* import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Scanner; */
-// <<<<<<
-
+/**
+ *  The {@code PercolationStats} class runs the Monte Carlo
+ *  simulation on {@code Percolation} class and calculates 
+ *  statistics like mean value of percolation threshold or
+ *  standard deviation;
+ *  <p>
+ *  For additional documentation, see
+ *  <a href="http://coursera.cs.princeton.edu/algs4/assignments/percolation.html"></a>
+ *  by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Volodymyr Shanoilo
+ */
 public class PercolationStats {
-// !!!!!! final
     private final int size;
     private final int trials;
     private final double mean;
     private final double deviation;
 
-// >>>>>>
-/*    private boolean randomIn = true;
-    private Scanner inFile = null;
-    private final String fileName = "test_data/input3.txtss";
-    private List<IntPairs> inputBuf = new ArrayList<IntPairs>();
-    
-    private class IntPairs {
-        public int row;
-        public int col;
-    } */
-// <<<<<<<<
-    
-    public PercolationStats(int n, int trialsIn) // perform trials independent experiments on an n-by-n grid
+/**
+ * Performs trials of independent experiments on an n-by-n grid
+ * @param n - size of the grid
+ * @param trialsIn - number of trials to run
+ */
+    public PercolationStats(int n, int trialsIn)
     {
         if ((n <= 0) || (trialsIn <= 0))
             throw new IllegalArgumentException("Bad input parameters: size = " + n + ", trials = " + trialsIn);
         
         trials = trialsIn;
         size = n;
-// >>>>>>>
-/*        if ((n == 0) && (trialsIn == 0)) {
-            try {
-                openFile(fileName);
-                System.out.println("Input is taken from " + fileName);
-                size = readFilePairs(inputBuf);
-                trials = 2;
-                randomIn = false;
-            } catch (FileNotFoundException nfex) {
-                size = 500;
-                trials = 10;
-                System.out.println("Falling back to random input with default " + size + " size and " + trials + " trials");
-            }
-        } else {
-            trials = trialsIn;
-            size = n;
-            System.out.println("Random input with user-provided " + size + " size and " + trials + " trials");
-        } */
-// <<<<<<<<
-        double[] threshold = new double[trials];
+        
+        double[] threshold = new double[trials];    // an array to store the results of each trial
         for (int i = 0; i < trials; i++) {
             Percolation perc = new Percolation(size);
             threshold[i] = singleRun(perc);
         }
+        // It would be easy and quick to calculate mean value right inside the loop
+        // In this case there would be no need in calling StdStats.mean() at all
+        // However the number of calls to StdStats methods is checked during the submission
+        // (both mean() and stddev() should be called exactly 1 time)
+        // This is the reason for calling the methods here
         mean = StdStats.mean(threshold);
         deviation = StdStats.stddev(threshold);
     }
-    
-// >>>>>>>>
-/*    private void openFile(String name) throws FileNotFoundException
-    {
-        FileReader file = null;
-        file = new FileReader(name);
-        inFile = new Scanner(file);
-    }
-    
-    private int readFilePairs(List<IntPairs> buf)
-    {
-        int len = inFile.nextInt();
-        
-        try {
-            while (true) {
-                IntPairs tmp = new IntPairs();
-                tmp.row = inFile.nextInt();
-                tmp.col = inFile.nextInt();
-                buf.add(tmp);
-            }
-        } catch (NoSuchElementException eof) {
-            // File ended
-        }
-        return len;
-    } */
-// <<<<<<<<<  
-    
+
+/**
+ * Run a single simulation
+ * Rows and columns are picked randomly
+ * @param perc - an initialized instance of a grid
+ * @return - percolation threshold for the current run
+ */
     private double singleRun(Percolation perc) {
         int row, col;
         int i = 0;
-// >>>>>>>>>
-//        if (randomIn) {
-// <<<<<<<<<  
+
         for (i = 0; i < size * size; i++) {
-                do {
+                do { // a loop to exclude already opened cells
                     row = StdRandom.uniform(1, size + 1);
                     col = StdRandom.uniform(1, size + 1);
                 } while (perc.isOpen(row, col));
@@ -106,27 +66,23 @@ public class PercolationStats {
                 if (perc.percolates())
                     break;
             }
-// >>>>>>>>>        
-/*      } else {
-            while (i < inputBuf.size())
-            {
-                IntPairs tmp = inputBuf.get(i++);
-    
-                perc.open(tmp.row, tmp.col);
-                if (perc.percolates())
-                    break;
-            }
-        } */
-// <<<<<<<<<
         return (double) perc.numberOfOpenSites() / (size * size);
     }
 
-    public double mean() // sample mean of percolation threshold
+/**
+ * Mean of percolation threshold
+ * @return
+ */
+    public double mean()
     {
         return mean;
     }
 
-    public double stddev() // sample standard deviation of percolation threshold
+/**
+ * Returns standard deviation of percolation threshold
+ * @return
+ */
+    public double stddev()
     {
         return deviation;
     }
@@ -135,16 +91,30 @@ public class PercolationStats {
         return 1.96 * stddev() / Math.sqrt(trials);
     }
 
-    public double confidenceLo() // low endpoint of 95% confidence interval
+/**
+ * Returns low endpoint of 95% confidence interval
+ * @return
+ */
+    public double confidenceLo()
     {
         return mean() - fivePercent();
     }
 
-    public double confidenceHi() // high endpoint of 95% confidence interval
+    /**
+     * Returns high endpoint of 95% confidence interval
+     * @return
+     */
+    public double confidenceHi()
     {
         return mean() + fivePercent();
     }
 
+/**
+ * Test client
+ * Uses input arguments. 
+ * @param args[0] - size of the grid
+ * @param args[1] - number of trials
+ */
     public static void main(String[] args) // test client (described below)
     {
         int size, trials;
